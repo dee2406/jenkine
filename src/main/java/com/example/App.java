d75d209d710d697ad0e8c4dea9f074827dc5783c
dee2406 <dishargowda246@gmail.com>
2025-12-23 12:57:06 +0530

    Add additional greeting message to App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Jenkins Maven Build");
         System.out.println("Hello");
+        System.out.println("Hello jiii");
     }
 }
